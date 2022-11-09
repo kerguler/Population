effc834875aa318a73e9f2cd55d0481ccd4f5d15
Kamil Erguler <kerguler@gmail.com>
2022-11-09 08:02:44 +0200

    draft

--- docs/examples/ex9/ex9.c
+++ docs/examples/ex9/ex9.c
@@ -6,7 +6,7 @@
 extern gsl_rng *RANDOM;
 
 void sim(char stoch, char id) {
-    int i, j, k, l;
+    int i, j, k;
 
     double death[5] = {0.07, 0.004, 0.003, 0.0025, 0.27};
     double develop[5] = {0.6, 5.0, 5.9, 4.1, 1e13};
@@ -19,11 +19,6 @@ void sim(char stoch, char id) {
     population pop[5];
     for (i=0; i<5; i++)
         pop[i] = spop2_init(arbiters, stoch);
-
-    population popdone[5][2];
-    for (i=0; i<5; i++)
-        for (j=0; j<2; j++)
-            popdone[i][j] = spop2_init(arbiters, stoch);
 
     number key[2] = {numZERO,numZERO};
     number num;
@@ -41,19 +36,16 @@ void sim(char stoch, char id) {
 
     number size[5], completed[5][2];
     double par[2];
-    member elm, tmp;
     number eggs;
 
     for (j=0; j<300*tau; j++) {
         for (i=0; i<5; i++) {
             par[0] = death[i] / tau;
             par[1] = develop[i] * tau;
-            spop2_step(pop[i], par, &size[i], completed[i], popdone[i]);
+            spop2_step(pop[i], par, &size[i], completed[i], 0);
 
             if (i) {
-                HASH_ITER(hh, popdone[i-1][1]->members, elm, tmp) {
-                    spop2_add(pop[i], key, elm->num);
-                }
+                spop2_add(pop[i], key,completed[i-1][1]);
             }
         }
 
@@ -70,17 +62,13 @@ void sim(char stoch, char id) {
         } else {
             printf("%d,%d",id,j); for (k=0; k<5; k++) printf(",%g",spop2_size(pop[k]).d); printf("\n");
         }
-
-        for (k=0; k<5; k++)
-            for (l=0; l<2; l++)
-                spop2_empty(&popdone[k][l]);
     }
 }
 
 int main(int attr, char *avec[]) {
     spop2_random_init();
 
-    if (FALSE)
+    if (TRUE)
         sim(DETERMINISTIC,0);
     else {
         int i;
