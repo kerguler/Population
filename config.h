fcd6e57244d7dd62c63de8a54faa9775b405038c
Kamil Erguler <kerguler@gmail.com>
2023-04-15 01:26:14 +0300

    memory leak fix release

--- config.h
+++ config.h
@@ -70,7 +70,7 @@
 #define PACKAGE_NAME "Population"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Population 0.0.4"
+#define PACKAGE_STRING "Population 0.0.5"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "population"
@@ -79,13 +79,13 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.0.4"
+#define PACKAGE_VERSION "0.0.5"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.0.4"
+#define VERSION "0.0.5"
 
 /* Define for Solaris 2.5.1 so the uint64_t typedef from <sys/synch.h>,
    <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
